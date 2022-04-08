418b1359cb47581c507df7fbf4918c38addf1b21
ArtisanGray <1697@holbertonschool.com>
2022-04-08 01:18:45 +0000

    created task 0, first test commit

---
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const data = JSON.parse(body).characters;
+  for (const car in data) {
+    request(data[car], (error, response, body) => {
+      if (error) {
+        console.error(error);
+      }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
