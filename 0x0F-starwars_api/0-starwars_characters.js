a73b1d5e62d75cd67a8f80f3f86ceb54dfcfe318
ArtisanGray <1697@holbertonschool.com>
2022-05-16 02:43:15 +0000

    fixed task 0, first test commit

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,17 +2,16 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) {
-    console.error(error);
-  }
-  const data = JSON.parse(body).characters;
-  data.forEach(lurl => {
-    request(lurl, (error, response, body) => {
-      if (error) {
-        console.error(error);
-      }
-      console.log(JSON.parse(body).name);
+request(url, async function (error, response, body) {
+  if (error) { console.error(error); }
+  const data = await JSON.parse(body).characters;
+  for (const charac of data) {
+    const name = await new Promise((resolve, reject) => {
+      request(charac, (error, response, body) => {
+        if (error) { console.error(error); }
+        resolve(JSON.parse(body).name);
+      });
     });
-  });
+    console.log(name);
+  }
 });
